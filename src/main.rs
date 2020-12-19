aab3d3990bfe7e5256afdf035830109346a9774e
aii-goto-yusaku <yusaku.g@inside.ai>
2020-12-19 22:27:36 +0900

    Add actix-web.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    r#"{"status":"ok"}"#
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
