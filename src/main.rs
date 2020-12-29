64317c9138a2d086e4de5f158c4b50f0c68f5503
aii-goto-yusaku <yusaku.g@inside.ai>
2020-12-29 17:51:55 +0900

    Add logging (bunnyan format).

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ mod core;
 
 use crate::core::error::Error;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use anyhow::{Context, Result};
 use serde::{Deserialize, Serialize};
+use tracing_actix_web::TracingLogger;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 
 #[get("/health")]
 async fn health() -> impl Responder {
@@ -25,14 +31,30 @@ async fn error(path_param: web::Path<ErrorEndpointPathParam>) -> Result<HttpResp
 }
 
 #[actix_web::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+async fn main() -> Result<()> {
     let config = config::Config::from_env().expect("Load configuration");
     println!("{:?}", config);
 
-    HttpServer::new(|| App::new().service(health).service(error))
-        .bind(format!("{}:{}", config.api_host, config.api_port))?
-        .run()
-        .await?;
+    LogTracer::init().expect("Unable to setup log tracer");
+    let app_name = concat!(env!("CARGO_PKG_NAME"), "-", env!("CARGO_PKG_VERSION")).to_string();
+    let (non_blocking_writer, _guard) = tracing_appender::non_blocking(std::io::stdout());
+    let bunyan_formatting_layer = BunyanFormattingLayer::new(app_name, non_blocking_writer);
+    let subscriber = Registry::default()
+        .with(EnvFilter::from_default_env())
+        .with(JsonStorageLayer)
+        .with(bunyan_formatting_layer);
+    tracing::subscriber::set_global_default(subscriber)
+        .context("Setting tracing subscriber global default")?;
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(TracingLogger)
+            .service(health)
+            .service(error)
+    })
+    .bind(format!("{}:{}", config.api_host, config.api_port))?
+    .run()
+    .await?;
 
     Ok(())
 }
